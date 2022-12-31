1d398030cd7523d5412fa7c9fda3b372503a40fb
koeldc <duttackoel@gmail.com>
2022-12-31 16:14:32 +0100

    replace preposition

--- preposition.py
+++ preposition.py
@@ -1,13 +1,3 @@
-#Substitute prepositions in the captions with randomly selected substitutes.
-# The captions are processed using the Spacy library, and noun phrases in the captions are detected using a custom function.
-# The preposition substitution is performed using a dictionary of substitutes.
-# Input:
-# - A text file containing a list of captions, one per line
-#- The input file is encoded in UTF-8
-# - The input file contains only valid English captions 
-# Output:
-# - A modified caption with prepositions replaced by substitutes
-
 import os
 import random
 import spacy
@@ -31,30 +21,43 @@ def detection_of_noun_phrases(doc):
         noun_phrases.append(' '.join(current_noun_phrase))
     return noun_phrases
 
-prep_substitute_dict = {
-    'in': {'at', 'on'},
-    'on': {'at', 'in'},
-    'at': {'in', 'on'},
-}
-
-real_captions_file = 'caption.txt'
-real_captions = []
-
-# Read in the list of captions from the 'new.en' file
-with open(real_captions_file, 'r') as f:
-    for line in f:
-        real_captions.append(line.strip())
-
-for p, caption in enumerate(real_captions):
-    print("p:", p)
-    substitution_cnt = 0
-    doc = nlp(caption.lower())
-    noun_phrases = detection_of_noun_phrases(doc)
+def substitute_prepositions(doc, prep_substitute_dict):
     words = [token.text for token in doc]
     for i, token in enumerate(doc):
         if token.pos_ == 'ADP' and token.text in prep_substitute_dict:
             substitute = random.choice(list(prep_substitute_dict[token.text]))
             words[i] = substitute
-    modified_caption = ' '.join(words)
-    print(modified_caption)
+    return ' '.join(words)
+
+def modify_captions(captions, prep_substitute_dict):
+    modified_captions = []
+    for caption in captions:
+        doc = nlp(caption.lower())
+        noun_phrases = detection_of_noun_phrases(doc)
+        modified_caption = substitute_prepositions(doc, prep_substitute_dict)
+        modified_captions.append(modified_caption)
+    return modified_captions
+
+def main():
+    prep_substitute_dict = {
+        'in': {'at', 'on'},
+        'on': {'at', 'in'},
+        'at': {'in', 'on'},
+    }
+
+    real_captions_file = 'input.txt'
+    real_captions = []
+
+    # Read in the list of captions from the file
+    with open(real_captions_file, 'r') as f:
+        for line in f:
+            real_captions.append(line.strip())
+
+    modified_captions = modify_captions(real_captions, prep_substitute_dict)
+    for p, modified_caption in enumerate(modified_captions):
+        print(f"p: {p}")
+        print(modified_caption)
+
+if __name__ == '__main__':
+    main()
 
